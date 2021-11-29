07b1d24e0af58e111902181e2758155e5310f0ef
Andre Encinas <andres.encinas@du.edu>
2021-11-28 20:48:30 -0700

    committing updated scrape python file

--- scraping.py
+++ scraping.py
@@ -8,59 +8,70 @@ import pandas as pd
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-#visit mars nasa news site
-url = 'http://redplanetscience.com'
-browser.visit(url)
+def mars_news(browser):
+    #visit mars nasa news site
+    url = 'http://redplanetscience.com'
+    browser.visit(url)
 
-#optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    #optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-#convert html to soup object
-html = browser.html
-news_soup = soup(html, 'html.parser')
+    #convert html to soup object
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-slide_elem = news_soup.select_one('div.list_text')
+    #add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+        slide_elem.find('div', class_='content_title')
+        #use parent element to find the first 'a' tag and save it as 'news_title
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        #use the parent element to tfind the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    
+    except AttributeError:
+        return None, None
 
-slide_elem.find('div', class_='content_title')
-
-#use parent element to find the first 'a' tag and save it as 'news_title
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
-
-#use the parent element to tfind the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    return news_title, news_p
 
 # ## JPL Space Images Featured Images
 
-#visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+def featured_image(browser):
+    #visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
-#find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    #find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-#parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    #parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-#find the relative image url
-img_url_rel = img_soup.find('img', class_='headerimage fade-in').get('src')
-img_url_rel
+    #add try/except for error handling
+    try:
+        #find the relative image url
+        img_url_rel = img_soup.find('img', class_='headerimage fade-in').get('src')
+    
+    except AttributeError:
+        return None
 
-#use the base url to create an absolute url
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    #use the base url to create an absolute url
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    
+    return img_url
 
 # ## Mars Facts
+def mars_facts():
+    #add try/except for error handling
+    try:
+        #use 'read_html' to scrape the facts table into a df
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    except BaseException:
+        return None
 
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
 
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
-
-df.to_html()
-
-browser.quit()
+    return df.to_html()
