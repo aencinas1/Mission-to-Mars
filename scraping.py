16a383ef0df69ae8f3a957a06cd20372a0d3139d
Andre Encinas <andres.encinas@du.edu>
2021-11-30 21:26:49 -0700

    committing module work

--- scraping.py
+++ scraping.py
@@ -3,10 +3,27 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
-#set up splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    #initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    #run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    #stop webdriver and return data
+    browser.quit()
+    return data
 
 def mars_news(browser):
     #visit mars nasa news site
@@ -75,3 +92,7 @@ def mars_facts():
     df.set_index('description', inplace=True)
 
     return df.to_html()
+
+if __name__ == "__main__":
+    #if running as script, print scraped data
+    print(scrape_all())
